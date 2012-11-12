019abd80af29340ce9db17dc4b81dceffee088dd
gongchenz <gongchenzhang@yahoo.com.cn>
2012-11-12 17:18:48 +0800

    creat project

---
+++ IOS_Demo/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  IOS_Demo
+//
+//  Created by apple  on 12-11-12.
+//  Copyright (c) 2012年 apple . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ IOS_Demo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  IOS_Demo
+//
+//  Created by apple  on 12-11-12.
+//  Copyright (c) 2012年 apple . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
